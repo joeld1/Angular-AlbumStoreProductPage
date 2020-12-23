dbcc5111fde200dcb6b69035a0fba362f53f7eee
joeld1 <>
2020-12-23 07:01:57 -0800

    Misc. Comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-
+// This bootstraps our application
 @NgModule({
   declarations: [
     AppComponent,
